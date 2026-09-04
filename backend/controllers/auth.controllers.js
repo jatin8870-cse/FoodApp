6a78176eb1ded805b2c997e874ff41a5a33d698d
Jatin <jy799914@gmail.com>
2026-09-04 15:27:38 +0530

    Fix Vercel React routing

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -74,7 +74,11 @@ export const singIn = async (req, res) => {
 
 export const signOut = async (req, res) => {
   try {
-    res.clearCookie("token");
+   res.clearCookie("token", {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none"
+    });
     return res.status(200).json({ message: "Signed out successfully" });
   } catch (error) {
     return res
