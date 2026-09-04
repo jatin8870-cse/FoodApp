dec1104db4e9072c7fcdb9dc0497f8f67e8b91eb
Jatin <jy799914@gmail.com>
2026-09-04 11:12:58 +0530

    Prepare backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true
 }));
 
