f67169b49c3f409568dec1e74a27e1ef83409096
Jatin <jy799914@gmail.com>
2026-09-08 22:15:03 +0530

    Fix Vercel React routing

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -236,12 +236,13 @@ export const googleSignIn = async (req, res) => {
         // Generate token
         const token = await genToken(user._id);
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: false, // localhost
-            sameSite: "lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000
-        });
+       res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/"
+});
 
         return res.status(200).json({
             message: "Google signin successful",
