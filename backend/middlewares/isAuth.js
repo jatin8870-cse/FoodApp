9beaac0726296353eff5353a790ea82deb57aa8d
Jatin <jy799914@gmail.com>
2026-09-08 22:05:42 +0530

    Fix Vercel React routing

--- backend/middlewares/isAuth.js
+++ backend/middlewares/isAuth.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 const isAuth = async (req, res, next) => {
     try {
 
@@ -35,3 +36,4 @@ const isAuth = async (req, res, next) => {
         });
     }
 };
+export default isAuth
