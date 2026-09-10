92a7b289d0be8691fb1da0059fc87f5ba84b0512
Jatin <jy799914@gmail.com>
2026-09-10 13:34:17 +0530

    Fix Vercel React routing

---
+++ frontend/src/components/CategoryCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function CategoryCard() {
+  return (
+    <div className='w-[120px] h-[140px] md:h-[180px] rounded-2xl
+    border-2 border-[#ff4d2d] shrink-0 overflow-hidden bg-white shadow-xl
+    shadow-gray-200 hover:shadow-lg transition-shadow '>
+      
+    </div>
+  )
+}
+
+export default CategoryCard;
--- frontend/src/components/UserDashboard.jsx
+++ frontend/src/components/UserDashboard.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Nav from './Nav'
+import CategoryCard from "./CategoryCard";
+import { categories } from "../categories";
 
 const UserDashboard = () => {
   return (
     <div>
-       <Nav/>
+      <Nav />
+      <div className="w-full max-w-6xl flex flex-col gap-5 items-start p-[10px]">
+        <h1 className='text-gray-800 text-2xl sm:text-3xl'>Inspiration for your first order </h1>
+        <div className='w-full '>
+        <div className='w-full flex overflow-x-auto gap-4 pb-2 scrollbar-thin scrollbar-thumb-[#ff4d2d] scrollbar-track-transparent scroll-smooth'>
+          {categories.map((cate, index) => (
+            <CategoryCard data={cate} key={index} />
+          ))}
+        </div>
+      </div>
     </div>
-  
+    </div>
+
   )
 }
 
